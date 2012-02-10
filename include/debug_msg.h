1ae909357469cf717aa564a0275b6a5d6cac0f89
Shawn the R0ck <citypw@gmail.com>
2012-02-10 11:00:26 +0800

    make debug macros easier to use, just like printf

--- include/debug_msg.h
+++ include/debug_msg.h
@@ -1,7 +1,8 @@
 /* Using these 2 macros instead of traditional printf */
 #define DEBUG_ERR(format, args...) fprintf(stderr, format, ##args)
 
-#define DEBUG_MSG(string) do{ \
+#define DEBUG_MSG(string, args...) do{ \
 	DEBUG_ERR("File: %s, Function: %s, Line: %d\nDescription: ", __FILE__, __FUNCTION__, __LINE__); \
-	DEBUG_ERR(string); \
+	DEBUG_ERR(string, ##args); \
+	DEBUG_ERR("-------------------------------\n"); \
 }while(0);
