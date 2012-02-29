79259ca7823586ec366a80d0f0f8d3353f80da23
Shawn the R0ck <citypw@gmail.com>
2012-02-29 12:46:04 +0800

    an easy way to put your libs into an archive

---
+++ unix_system_programming/create_archives/run.c
@@ -0,0 +1,14 @@
+/**
+ * This code is GPL'ed!
+ * monkey-coder:Shawn the R0ck <citypw@gmail.com>
+ */
+
+#include <stdio.h>
+#include "lib.h"
+
+int main()
+{
+  integer(25);
+  pass_str("hi there dude!");
+  return 0;
+}
---
+++ unix_system_programming/create_archives/test2.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+void pass_str(char* str)
+{
+  printf("got a string:%s\n",str);
+}
