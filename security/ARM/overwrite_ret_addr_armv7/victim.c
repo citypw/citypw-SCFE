30c2b73f85e38582947f5458085988787aaa9216
Shawn the R0ck <citypw@gmail.com>
2013-11-21 01:23:24 +0800

    ret_addr_armv7

---
+++ security/ARM/overwrite_ret_addr_armv7/victim.c
@@ -0,0 +1,25 @@
+/*
+ * MOnkey-coder: Shawn the R0ck@Copyleft
+ * This sample code is GPL'ed!
+ */
+#include <stdio.h>
+#include <string.h>
+
+void test(const char *input)
+{
+  char buf[10]; /* buf[1024] for testing exploit */
+  strcpy(buf, input);
+  printf("%s \n", buf);
+}
+
+void fuck_me(void)
+{
+  printf("being hacked\n");
+}
+
+int main(int argc, char *argv[])
+{
+  printf("The address of func test(): %p, func fuck_me(): %p\n", test, fuck_me);
+  test(argv[1]);
+  return 0;
+}
