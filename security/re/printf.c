2ad046bba8f078ef458b015caaab1a504a436c0d
Shawn the R0ck <citypw@gmail.com>
2014-06-05 18:02:20 +0800

    re

---
+++ security/re/printf.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main()
+{
+        printf("a=%d; b=%d; c=%d", 1, 2, 3);
+        return 0;
+}
---
+++ security/re/printf.s
@@ -0,0 +1,32 @@
+	.file	"printf.c"
+	.intel_syntax noprefix
+	.section	.rodata
+.LC0:
+	.string	"a=%d; b=%d; c=%d"
+	.text
+	.globl	main
+	.type	main, @function
+main:
+.LFB0:
+	.cfi_startproc
+	push	rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	mov	rbp, rsp
+	.cfi_def_cfa_register 6
+	mov	eax, OFFSET FLAT:.LC0
+	mov	ecx, 3
+	mov	edx, 2
+	mov	esi, 1
+	mov	rdi, rax
+	mov	eax, 0
+	call	printf
+	mov	eax, 0
+	pop	rbp
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	main, .-main
+	.ident	"GCC: (Ubuntu/Linaro 4.6.3-1ubuntu5) 4.6.3"
+	.section	.note.GNU-stack,"",@progbits
